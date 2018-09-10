e0e57bc63191410ae801c414c1e4c5a684dc36a7
dudu <dudupo51@gmail.com>
2018-09-10 17:41:13 +0300

    compiling the tex on the machine

--- cnote.sh
+++ cnote.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mdfile=`cat $1 | marked`
-./template.sh $mdfile > "${1%.*}.html"
+./template.sh $mdfile > "temp${1%.*}.html"
+mjpage --dollars < "temp${1%.*}.html" > "${1%.*}.html"
+rm "temp${1%.*}.html"

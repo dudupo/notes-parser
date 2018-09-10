441e8e66e78bdefe119bbc3e20b063869a8c1fb1
dudu <dudupo51@gmail.com>
2018-09-10 19:10:00 +0300

    ~update template~

--- cnote.sh
+++ cnote.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+branch=`git branch`
 git checkout temp
 templatediff=`git diff ./template.html`
 if [[ ! -z "$templatediff" ]]; then
   echo -e "template have changed :"
   git diff ./template.html
   ./build.sh
-  git add ./template.html
+  git add ./template.html ./template.sh
+  git commit -m "~update template~"
+  git checkout $branch
+  git merge temp
 fi
 
 ./premark.sh $1
